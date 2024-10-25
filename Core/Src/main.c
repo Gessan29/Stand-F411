26a6f7b6965744ace1e7f0df094d2978990d16f1
Gessan29 <gessan29@gmail.com>
2024-10-25 18:17:01 +0700

    передача данных АЦП на компьютер
    включил АЦП для измерения напряжения на потенциометр, с последующей передачей на компьютер через хаб UART-USB.
    Не удалось в массив передать значение контстаны типы float.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,8 +21,9 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#define VOL 6
 #define BUFFER_SIZE 50
+#include "string.h"
 #include <stdio.h>
 int a = 0;
 
@@ -45,7 +46,6 @@ int a = 0;
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
-DMA_HandleTypeDef hdma_adc1;
 
 UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
@@ -53,10 +53,11 @@ UART_HandleTypeDef huart6;
 
 /* USER CODE BEGIN PV */
 
-
-
+int voltage;
+uint16_t rawValue;
 uint8_t GPS [] = "$GNGLL,5502.49000,N,08256.07600,E,          ,A,A*"; // GLL, version 4.1 and 4.2, NMEA 0183
-uint8_t Priem [BUFFER_SIZE] = {0};
+uint8_t Priem [BUFFER_SIZE];
+uint8_t USB [];
 
 
 /* USER CODE END PV */
@@ -64,7 +65,6 @@ uint8_t Priem [BUFFER_SIZE] = {0};
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_DMA_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_USART6_UART_Init(void);
@@ -123,7 +123,6 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_DMA_Init();
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
   MX_USART6_UART_Init();
@@ -131,41 +130,8 @@ int main(void)
   /* USER CODE BEGIN 2 */
 
 
-  switch (a){
-           case 0:
-        	   uint8_t Time1 [] = "120000.000";
-        	   update_gps_time(GPS, Time1);
+  HAL_ADC_Start(&hadc1);
 
-        	     HAL_UART_Receive (&huart1, (uint8_t*)Priem, BUFFER_SIZE, HAL_MAX_DELAY );
-                 HAL_UART_Transmit (&huart2, (uint8_t*)GPS, BUFFER_SIZE, HAL_MAX_DELAY );
-
-
-        	   a++;
-        	   HAL_Delay(1000);
-
-
-
-           case 1:
-               	   uint8_t Time2 [] = "120001.000";
-               	   update_gps_time(GPS, Time2);
-
-               	   HAL_UART_Receive (&huart1, (uint8_t*)Priem, BUFFER_SIZE, HAL_MAX_DELAY );
-               	   HAL_UART_Transmit (&huart2, (uint8_t*)GPS, BUFFER_SIZE, HAL_MAX_DELAY );
-
-               	   a++;
-               	   HAL_Delay(1000);
-
-           case 2:
-        	           uint8_t Time3 [] = "120002.000";
-        	           update_gps_time(GPS, Time3);
-               	   HAL_UART_Receive (&huart1, (uint8_t*)Priem, BUFFER_SIZE, HAL_MAX_DELAY );
-               	   HAL_UART_Transmit (&huart2, (uint8_t*)GPS, sizeof(GPS) - 1, HAL_MAX_DELAY );
-
-               	   HAL_Delay(1000);
-               	   a = 0;
-               	   break;
-
-    }
 
 
 
@@ -176,8 +142,14 @@ int main(void)
   while (1)
   {
 
-		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_9);
-		HAL_Delay(500);
+	    		  HAL_ADC_PollForConversion (&hadc1, HAL_MAX_DELAY);
+	    		  HAL_Delay (500);
+	    		  rawValue = HAL_ADC_GetValue (&hadc1);
+	    		  voltage = rawValue * 3.3 / 4095;
+
+
+	    		  HAL_UART_Transmit ( &huart1, (uint8_t*)USB, sizeof(USB), HAL_MAX_DELAY );
+
 
     /* USER CODE END WHILE */
 
@@ -253,15 +225,15 @@ static void MX_ADC1_Init(void)
   hadc1.Instance = ADC1;
   hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = ENABLE;
-  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 6;
+  hadc1.Init.NbrOfConversion = 1;
   hadc1.Init.DMAContinuousRequests = DISABLE;
-  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc1.Init.EOCSelection = ADC_EOC_SEQ_CONV;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {
     Error_Handler();
@@ -271,52 +243,7 @@ static void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_0;
   sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
-  */
-  sConfig.Channel = ADC_CHANNEL_1;
-  sConfig.Rank = 2;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
-  */
-  sConfig.Channel = ADC_CHANNEL_4;
-  sConfig.Rank = 3;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
-  */
-  sConfig.Channel = ADC_CHANNEL_5;
-  sConfig.Rank = 4;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
-  */
-  sConfig.Channel = ADC_CHANNEL_6;
-  sConfig.Rank = 5;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
-  */
-  sConfig.Channel = ADC_CHANNEL_7;
-  sConfig.Rank = 6;
+  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
@@ -427,22 +354,6 @@ static void MX_USART6_UART_Init(void)
 }
 
 /**
-  * Enable DMA controller clock
-  */
-static void MX_DMA_Init(void)
-{
-
-  /* DMA controller clock enable */
-  __HAL_RCC_DMA2_CLK_ENABLE();
-
-  /* DMA interrupt init */
-  /* DMA2_Stream0_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
-
-}
-
-/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -487,6 +398,10 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+
+
+
+
 
 /* USER CODE END 4 */
 
